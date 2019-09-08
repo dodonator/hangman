8d709a6bf8f1f386920e02d67fb168e98b07236d
dodo <dodo@chaosdorf.de>
2019-09-08 12:19:35 +0200

    word_sample() soll eine Stichprobe an Wörtern extrahieren, die der Computer gegen den Spieler benutzen kann.

---
+++ singleplayer.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from backend import word_score
+import random
+
+
+def word_sample(number=1000, limit=1000):
+    path = Path("words.txt")
+
+    wordlist = []
+    with path.open("r") as fo_r:
+        for line in fo_r:
+            if len(wordlist) < number:
+                word = line.strip()
+                score = word_score(word)
+                r = int(random.random() * limit)
+                if int(score) == r:
+                    wordlist.append(word)
+
+    return wordlist
