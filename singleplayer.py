a767b8c0684a5e55eb6a1a997682b06520d4ff40
dodo <dodo@chaosdorf.de>
2019-09-08 14:09:15 +0200

    Diese Datei wird nicht weiter benutzt

--- singleplayer.py
+++ singleplayer.py
@@ -3,27 +3,6 @@ from backend import word_score, convert
 from typing import List
 import random
 
-
-SIZE = 1000
-LIMIT = 1000
-
-def word_sample(size:int=SIZE, limit:int=LIMIT) -> List[str]:
-    path = Path("words.txt")
-
-    wordlist = []
-    with path.open("r") as fo_r:
-        for line in fo_r:
-            if len(wordlist) < number:
-                word = line.strip()
-                score = word_score(word)
-                r = int(random.random() * limit)
-                if int(score) == r:
-                    wordlist.append((word, score))
-
-    wordlist.sort(key=lambda x: x[1], reverse=True)
-    return wordlist
-
-
 def game():
     score = 0
     sample = word_sample()
