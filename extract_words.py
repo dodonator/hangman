5e0e04d8f14bd1e319394ee23d1f6a6f75858d43
dodo <dodo@chaosdorf.de>
2019-09-08 12:04:06 +0200

    extract_words: doppelte Worte werden entfernt

--- extract_words.py
+++ extract_words.py
@@ -6,15 +6,21 @@ print(f"the limit is {LIMIT}")
 
 path = Path("wordlist.txt")
 path_filtered = Path("words.txt")
+
 word_counter = 0
+last_word = ""
 with path.open("r") as fo_r:
     with path_filtered.open("w") as fo_a:
         for line in fo_r:
             word = line.strip()
+            if word.lower() == last_word.lower():
+                print("Wiederholung gefunden!")
+                continue
             score = word_score(word)
             if score <= LIMIT:
                 print(f"{score:<8} : {word}")
                 fo_a.write(word + "\n")
                 word_counter += 1
+            last_word = word
 
 print(f"\n{word_counter} words extracted")
